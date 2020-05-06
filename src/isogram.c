3412bde35c717bc72c63c19f389ff77cee78804d
ruejo <johannes.ruef@students.fhv.at>
2020-05-06 10:56:26 +0000

    implemet function and tests

--- src/isogram.c
+++ src/isogram.c
@@ -1,7 +1,21 @@
 #include "isogram.h"
+#include "string.h"
 
 
 bool is_isogram(const char phrase[]){
-
-    return false;
+    int length = strlen(phrase);
+    for (int i = 0; i<length; i++){
+        char currentChar = phrase[i];
+        for (int j=0; j<length; j++){
+            int occurences = 0;
+            if ((phrase[j] == currentChar) && (j != i)){
+                occurences++;
+            }
+            if (occurences > 0){
+                return false;
+            }
+        }
+    }
+    
+    return true;
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -1,5 +1,6 @@
 #include "vendor/unity.h"
 #include "../src/isogram.h"
+#include "vendor/unity_internals.h"
 #include <stdlib.h>
 
 void test_empty_string(void)
@@ -7,11 +8,23 @@ void test_empty_string(void)
    TEST_ASSERT_TRUE(is_isogram(""));
 }
 
+void test_aa_is_no_isogramm(){
+    bool result = is_isogram("aa");
+    TEST_ASSERT_FALSE(result);
+}
+
+void test_abc_is_no_isogramm(){
+    bool result = is_isogram("abc");
+    TEST_ASSERT_FALSE(result);
+}
+
 int main(void)
 {
    UnityBegin("isIsogram");
 
    RUN_TEST(test_empty_string);
+   RUN_TEST(test_aa_is_no_isogramm);
+   RUN_TEST(test_abc_is_no_isogramm);
 
    UnityEnd();
    return 0;
